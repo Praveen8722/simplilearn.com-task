725357398820ae910e7fbfd269aeb8f811861717
Praveen Hanamantagoudr <146224076+Praveen8722@users.noreply.github.com>
2024-04-17 21:51:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Get a reference to the button element
+    var button = document.getElementById('clickMeButton');
+
+    // Add a click event listener to the button
+    button.addEventListener('click', function () {
+        alert('Button clicked!');
+    });
+});
+
+
+
